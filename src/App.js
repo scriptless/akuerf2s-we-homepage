dd045295f5b3e90f3523053cde2452a52a7ce289
Alexander Kuerfgen <alexander.kuerfgen@smail.inf.h-brs.de>
2021-12-20 15:06:44 +0100

    Add tailwindcss (CSS utility framework)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-xl text-red">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
