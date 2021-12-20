7b1ffcd68475bbeccf2e555546126f66364b0e41
Alexander Kuerfgen <alexander.kuerfgen@smail.inf.h-brs.de>
2021-12-20 15:52:27 +0100

    Add content block

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import ContentBlock from "./components/content/ContentBlock";
 import Navbar from "./components/nav/Navbar";
 
 function App() {
   return (
     <div className="App h-screen bg-image">
       <Navbar/>
+      <div className="max-w-6xl mx-auto">
+        <ContentBlock/>
+      </div>
     </div>
   );
 }
--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -3,13 +3,14 @@ import SubNavbar from "./SubNavbar";
 export default function Navbar() {
     return (<>
         <div className="h-16 sticky top-0 bg-blue-600 shadow-lg w-full">
-            <div className="h-full max-w-6xl mx-auto">
-                <ul className="space-x-12 h-full flex items-center text-xl">
+            <div className="h-full max-w-6xl mx-auto flex justify-between items-center">
+                <ul className="space-x-12 flex items-center text-xl">
                     <a className="cursor-pointer flex items-center font-semibold text-gray-300 hover:text-gray-100">HTML</a>
                     <a className="cursor-pointer flex items-center font-semibold text-gray-300 hover:text-gray-100">CSS</a>
                     <a className="cursor-pointer flex items-center font-semibold text-white italic">JavaScript</a>
                     <a className="cursor-pointer flex items-center font-semibold text-gray-300 hover:text-gray-100">Other</a>
                 </ul>
+                <p className="text-white">Solution-Navigator v1.0</p>
             </div>
         </div>
         <SubNavbar/>
